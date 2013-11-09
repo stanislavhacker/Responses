f177d419c5cdd50dafc8347cc5e44462a3047022
Stanilav Hacker <info@dbsys.cz>
2013-11-09 20:45:18 +0100

    JSON Responses

---
+++ Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Responses
+{
+    public class Car
+    {
+        public String Manufacturer { get; set; }
+        public String Model { get; set; }
+        public String Year { get; set; }
+        public String Performance { get; set; }
+        public String Content { get; set; }
+        public String HolderName { get; set; }
+        public String Vin { get; set; }
+    }
+}
