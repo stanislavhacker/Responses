d3fa383965358a5883257a6153a14b66972552b6
Stanilav Hacker <info@dbsys.cz>
2013-11-10 11:48:35 +0100

    Pridana access token definice

---
+++ AccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Responses
+{
+    public class AccessToken
+    {
+        public String key { get; set; }
+        public DateTime valid { get; set; }
+    }
+}
